2ed25ca84c3ff7816a41c73ecc3ad18eedd8a07a
yunlou1995 <lizhuo1995pro@gmail.com>
2018-11-07 14:31:35 +0800

    1

--- upload.sh
+++ upload.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-VersionString=`grep -E 's.version.*=' A.podspec`
+VersionString=`grep -E 's.version.*=' A_Category.podspec`
 VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 
 NewVersionNumber=$(($VersionNumber + 1))
-LineNumber=`grep -nE 's.version.*=' A.podspec | cut -d : -f1`
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" A.podspec
+LineNumber=`grep -nE 's.version.*=' A_Category.podspec | cut -d : -f1`
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" A_Category.podspec
 
 echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
 
@@ -13,5 +13,5 @@ git add .
 git commit -am ${NewVersionNumber}
 git tag ${NewVersionNumber}
 git push origin master --tags
-pod repo push mediatorPods A.podspec --verbose --allow-warnings --use-libraries --use-modular-headers
+pod repo push mediatorPods A.podspec --verbose --allow-warnings --use-libraries
 
